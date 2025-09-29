df24b6148bd011c56e305e51bc1ea2a2c7f1dd90
Oein <oein@oein.kr>
2025-09-29 14:42:23 +0900

    2025.09.29

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,4 @@
-import { Deepslate, DeepslateFS_Local } from "#";
+import { Deepslate, DeepslateFS_Local, type DeepslatePlugin } from "#";
 
 const app = new Deepslate({
   port: 3000,
@@ -6,5 +6,19 @@ const app = new Deepslate({
     fs: new DeepslateFS_Local("./data"),
   },
 });
+
+// Example plugin
+
+class HelloWorld implements DeepslatePlugin {
+  name = "HelloWorld";
+  version = "1.0.0";
+  init(deepslate: Deepslate) {
+    deepslate.server.get("/", (req, res) => {
+      res.send("Hello, World!");
+    });
+  }
+}
+
+app.use(new HelloWorld());
 
 await app.start();
